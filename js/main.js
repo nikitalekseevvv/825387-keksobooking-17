ebb46f6e595900297475fc15588e429fa1fcfc75
nikitalekseevvv <supersonicrock2@gmail.com>
2019-06-14 12:50:50 +0400

    Исправил замения кроме функции сбора массива

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 'use strict';
 
 var TYPES_OF_ADS = ['palace', 'flat', 'house', 'bungalo'];
-var PIN_WIDTH = 50;
 var PIN_HEIGHT = 165;
-var MIN_X = 0 + PIN_WIDTH / 2;
-var MAX_X = 1200 - PIN_WIDTH / 2;
+var MIN_X = 0;
+var MAX_X = 1200;
 var MIN_Y = 130 + PIN_HEIGHT / 2;
 var MAX_Y = 630 - PIN_HEIGHT / 2;
 var NUBMERS_OF_ADS = 8;
 var map = document.querySelector('.map');
-var pin = document.querySelector('#pin').content.querySelector('.map__pin');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
 // Получение рандомного значения
 var getRandomNumber = function (min, max) {
@@ -18,7 +17,7 @@ var getRandomNumber = function (min, max) {
 
 // Получение рандомного элемента
 var getRandomElement = function (elements) {
-  return elements[Math.floor(Math.random() * elements.length)];
+  return elements[getRandomNumber(0, elements.length)];
 };
 
 // Генерация аватара
@@ -27,18 +26,21 @@ var getAvatar = function (index) {
   return 'img/avatars/user' + '0' + index + '.png';
 };
 
-// Создание объявление
-var createAd = function (arr) {
-  var ad = {
-    author: {avatar: getAvatar(arr)},
-    offer: {type: getRandomElement(TYPES_OF_ADS)},
-    location: {
-      x: getRandomNumber(MIN_X, MAX_X),
-      y: getRandomNumber(MIN_Y, MAX_Y)
-    }
-  };
+// Создание массива объявлений
+var createAds = function (index) {
+  var adsList = [];
 
-  return ad;
+  for (var i = 0; i <= index; i++) {
+    adsList.push({
+      author: {avatar: getAvatar(i)},
+      offer: {type: getRandomElement(TYPES_OF_ADS)},
+      location: {
+        x: getRandomNumber(MIN_X, MAX_X),
+        y: getRandomNumber(MIN_Y, MAX_Y)
+      }
+    });
+  }
+  return adsList;
 };
 
 // Задание 2. Активация карты
@@ -53,28 +55,26 @@ activateMap();
 
 // Генерация метки объявления
 var renderPin = function (ad) {
-  // Объявление и разрешение на клониование всего узла
-  var pinTemplate = pin.cloneNode(true);
-  // Указание аватара
-  pinTemplate.querySelector('img').src = ad.author.avatar;
-  // Указание расположения
-  pinTemplate.style = 'left: ' + (ad.location.x) + 'px; top: ' + (ad.location.y) + 'px;';
-  // Указание альтернативного значения
-  pinTemplate.querySelector('img').alt = 'Заголовок объявления';
-  // Возвращение метки объявления
-  return pinTemplate;
+  var templateCopy = pinTemplate.cloneNode(true);
+
+  templateCopy.querySelector('img').src = ad.author.avatar;
+  templateCopy.style.left = ad.location.x + 'px';
+  templateCopy.style.top = ad.location.y + 'px';
+  templateCopy.querySelector('img').alt = 'Заголовок объявления';
+
+  return templateCopy;
 };
 
 // Задание 4. Отрисовка объявлений
 
 // Функция для показа объявлений на карте
-var showAds = function () {
+var showAds = function (index) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < NUBMERS_OF_ADS; i++) {
-    var similarAd = createAd(i);
+  for (var i = 0; i < index; i++) {
+    var similarAd = createAds(i);
     fragment.appendChild(renderPin(similarAd));
   }
   map.appendChild(fragment);
 };
 
-showAds();
+showAds(NUBMERS_OF_ADS);
