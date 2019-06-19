fd44716745f774f7b9991228b50c4313650c1022
Никита <supersonicrock2@gmail.com>
2019-06-19 21:27:12 +0400

    Остановить вакханалию с движением пина

--- js/main.js
+++ js/main.js
@@ -180,3 +180,48 @@ function onChangeTimeInput(evt) {
 
 timeIn.addEventListener('change', onChangeTimeInput);
 timeOut.addEventListener('change', onChangeTimeInput);
+
+// ----------------ЗАДАНИЕ 9 ---------------------
+
+buttonMapPin.addEventListener('mousedown', function(evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var currentCoords = {
+      x: buttonMapPin.offsetLeft - shift.x,
+      y: buttonMapPin.offsetTop - shift.y
+    };
+
+    buttonMapPin.style.left = currentCoords.x + 'px';
+    buttonMapPin.style.top = currentCoords.y + 'px';
+
+    setAddress(currentCoords.x, currentCoords.y)
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
