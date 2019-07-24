b43f452ef4de375d0d2276c78323f393582d6712
nikitalekseevvv <supersonicrock2@gmail.com>
2019-07-24 12:14:36 +0400

    Исправление замечаний

--- js/message.js
+++ js/message.js
@@ -6,37 +6,35 @@
 
   // Отрисовка окна
   var drawPopup = function (template, section) {
-    var popup = template.clonNode(true);
+    var popup = template.cloneNode(true);
 
     // Закрытие окна
     var onPopupClose = function () {
       section.removeChild(popup);
+      document.removeEventListener('click', onPopupClose);
     };
 
     // Закрытие окна по нажатию Esc
     var onPopupEscPress = function (evt) {
       window.utils.isEscEvent(evt, onPopupClose);
+      document.removeEventListener('keydown', onPopupEscPress);
     };
 
-    section.addEventListener('keydown', onPopupEscPress);
-    section.addEventListener('click', onPopupClose);
+    document.addEventListener('keydown', onPopupEscPress);
+    document.addEventListener('click', onPopupClose);
 
     return popup;
   };
 
   window.message = {
     showError: function (errorMessage) {
-      var fragment = document.createDocumentFragment();
       var errorPopup = drawPopup(errorTemplate, mainSection);
       errorPopup.querySelector('.error__message').textContent = errorMessage;
-      fragment.appendChild(errorPopup);
-      mainSection.appendChild(fragment);
+      mainSection.appendChild(errorPopup);
     },
     showSuccess: function () {
-      var fragment = document.createDocumentFragment();
       var successPopup = drawPopup(successTemplate, mainSection);
-      fragment.appendChild(successPopup);
-      mainSection.appendChild(fragment);
+      mainSection.appendChild(successPopup);
     }
   };
 
